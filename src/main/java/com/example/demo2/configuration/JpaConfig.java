a9debd46113600b4f3678e4caf72033937dff797
Jay Ehsaniara <ehsaniara@gmail.com>
2022-05-05 13:43:54 -0700

    Add Entity Audit feature:
      + Add EnableJpaAuditing in JpaConfig class
      + Defined EntityAudit class and extend Entities on

---
+++ src/main/java/com/example/demo2/configuration/JpaConfig.java
@@ -0,0 +1,9 @@
+package com.example.demo2.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@Configuration
+public class JpaConfig {
+}
