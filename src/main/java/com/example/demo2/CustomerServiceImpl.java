99aeb0337498573bcb4a04424eb932eb6926d5cb
Jay Ehsaniara <ehsaniara@gmail.com>
2022-05-11 12:22:05 -0700

    service methods verification

--- src/main/java/com/example/demo2/CustomerServiceImpl.java
+++ src/main/java/com/example/demo2/CustomerServiceImpl.java
@@ -2,6 +2,7 @@ package com.example.demo2;
 
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
 
 @Service
 @AllArgsConstructor
@@ -11,10 +12,13 @@ public class CustomerServiceImpl implements CustomerService {
     private final CustomerMapper customerMapper;
 
     public CustomerDto createCustomer(CustomerCreateDto customerCreateDto) {
+        Assert.notNull(customerCreateDto, "customerCreateDto can not be null");
+        Assert.notNull(customerCreateDto.getCustomerName(), "customerName can not be null");
         return customerMapper.customerToDto(customerRepository.save(CustomerEntity.builder().customerName(customerCreateDto.getCustomerName()).build()));
     }
 
     public String greeting(String name) {
+        Assert.notNull(name, "Name can not be null");
         return String.format("Hello %s!", name);
     }
 }
--- src/test/java/com/example/demo2/CustomerServiceImplTest.java
+++ src/test/java/com/example/demo2/CustomerServiceImplTest.java
@@ -3,15 +3,13 @@ package com.example.demo2;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mapstruct.factory.Mappers;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.util.ReflectionTestUtils;
 
-import static org.mockito.Mockito.any;
-import static org.mockito.Mockito.doReturn;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class CustomerServiceImplTest {
@@ -27,6 +25,7 @@ class CustomerServiceImplTest {
 
     @Test
     void testCreateCustomer() {
+
         var customerEntity = CustomerEntity.builder().customerId(1L).customerName("Jay").build();
 
         var customerDto = CustomerDto.builder()//
@@ -34,22 +33,46 @@ class CustomerServiceImplTest {
                 .customerName(customerEntity.getCustomerName())//
                 .build();
 
-        doReturn(customerEntity).when(customerRepository).save(any());
-
-        doReturn(customerDto).when(customerMapper).customerToDto(any());
-
         var customerCreateDto = CustomerCreateDto.builder()//
                 .customerName(customerEntity.getCustomerName())//
                 .build();
 
+        when(customerRepository.save(any(CustomerEntity.class))).thenReturn(customerEntity);
+
+        doReturn(customerDto).when(customerMapper).customerToDto(customerEntity);
+
         var result = customerService.createCustomer(customerCreateDto);
         Assertions.assertNotNull(result);
 
-        var resultDto = customerMapper.customerToDto(customerEntity);
-        Assertions.assertNotNull(resultDto);
+        verify(customerRepository, times(1)).save(any());
 
-        Assertions.assertEquals(customerDto.getCustomerName(), resultDto.getCustomerName());
-        Assertions.assertEquals(customerDto.getCustomerId(), resultDto.getCustomerId());
+        //almost same as last verify
+        ArgumentCaptor<CustomerEntity> personArgument = ArgumentCaptor.forClass(CustomerEntity.class);
+        verify(customerRepository, times(1)).save(personArgument.capture());
+        verifyNoMoreInteractions(customerRepository);
+
+        //comparing the result with what it should be
+        assertEquals(customerDto, result);
+    }
+
+    @Test
+    void testCreateCustomer_NullObject() {
+        IllegalArgumentException thrown = assertThrows(
+                IllegalArgumentException.class,
+                () -> customerService.createCustomer(null),
+                "IllegalArgumentException was expected when object is null"
+        );
+        assertTrue(thrown.getMessage().contains("customerCreateDto can not be null"));
+    }
+
+    @Test
+    void testCreateCustomer_NullName() {
+        IllegalArgumentException thrown = assertThrows(
+                IllegalArgumentException.class,
+                () -> customerService.createCustomer(CustomerCreateDto.builder().build()),
+                "IllegalArgumentException was expected when name is null"
+        );
+        assertTrue(thrown.getMessage().contains("customerName can not be null"));
     }
 
     @Test
@@ -57,5 +80,15 @@ class CustomerServiceImplTest {
         var result = customerService.greeting("Jay");
         Assertions.assertNotNull(result);
         Assertions.assertEquals("Hello Jay!", result);
+    }
+
+    @Test
+    void testGreeting_NullName() {
+        IllegalArgumentException thrown = assertThrows(
+                IllegalArgumentException.class,
+                () -> customerService.greeting(null),
+                "IllegalArgumentException was expected when name is null"
+        );
+        assertTrue(thrown.getMessage().contains("Name can not be null"));
     }
 }
