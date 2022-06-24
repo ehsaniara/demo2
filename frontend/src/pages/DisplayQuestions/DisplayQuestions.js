706376f839f8974664b85602a050c6d050a10b7e
Angie D <Dangie@vmware.com>
2022-06-24 14:24:50 -0600

    css

--- frontend/src/pages/DisplayQuestions/DisplayQuestions.js
+++ frontend/src/pages/DisplayQuestions/DisplayQuestions.js
@@ -1,9 +1,10 @@
+import './DisplayQuestions.css'
+
 export default function DisplayQuestions({questions}) {
     const renderQuestions = () => {
-        console.log(questions)
         if (questions.length === 0) return <h2>Submit a question</h2>
         return (
-            <>
+            <div className="display-questions-body">
                 <h2>Pick a question to try or submit a new question</h2>
                 {questions.map(question => (
                         <div key={question.questionTemplateUuid}>
@@ -14,7 +15,7 @@ export default function DisplayQuestions({questions}) {
                     )
                 )
                 }
-            </>
+            </div>
         )
     }
 
